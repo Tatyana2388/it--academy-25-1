c23eb16f81c616bcec9e9d251ad077021e95c94f
Tatiana Karpovich <tanysha.karpovich@gmail.com>
2025-02-25 20:30:43 +0300

    Basics commit

---
+++ homework04/explicit.js
@@ -0,0 +1,14 @@
+// number conversion
+let num1 = Boolean(0);
+console.log(num1)
+
+//string conversion
+let num2 = parseInt('10', 10);
+console.log(num2)
+
+// boolean conversion
+let num3 = String(true);
+console.log(num3)
+
+
+

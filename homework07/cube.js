14cbbc2261e918e514dd965413aa8cc4c0731ef5
Tatiana Karpovich <tanysha.karpovich@gmail.com>
2025-03-10 22:32:34 +0300

    CR changes

--- homework07/cube.js
+++ homework07/cube.js
@@ -1,23 +1,27 @@
-const playersCount = 5;
-const attempts = 10;
-const results = [];
+const game = (playersCount, attempts ) => {
 
-const getRandomInt = () => Math.ceil(Math.random() * 6);
-const calcPlayerResult = () => Array(attempts).fill(0).reduce((res) => res + getRandomInt(), 0);
+    const results = [];
 
-for (let i = 0; i < playersCount; i++) {
+    const getRandomInt = () => Math.ceil(Math.random() * 6);
+    const calcPlayerResult = () => Array(attempts).fill(0).reduce((res) => res + getRandomInt(), 0);
 
-    results.push(calcPlayerResult());
+    for (let i = 0; i < playersCount; i++) {
+
+        results.push(calcPlayerResult());
+
+    }
+
+    const maxResult = Math.max(...results);
+    const onlyOneWinner = results.filter((res) => res === maxResult).length === 1;
+
+    console.log(results);
+    if (onlyOneWinner) {
+        const winner = results.indexOf(maxResult);
+        console.log(`Победил игрок ${winner + 1}`);
+    } else {
+        console.log('Ничья')
+    }
 
 }
 
-const maxResult = Math.max(...results);
-const onlyOneWinner = results.filter((res) => res === maxResult).length === 1;
-
-console.log(results);
-if (onlyOneWinner) {
-    const winner = results.indexOf(maxResult);
-    console.log(`Победил игрок ${winner + 1}`);
-} else {
-    console.log('Ничья')
-}
+game(5,10);
